c549528fd7d6dd1f5dbbc88fdc9516f8109eb88b
Daniil Dryzhov <danildryzov@gmail.com>
2026-01-17 13:08:50 +0100

    Initial commit: initialize Spring Boot project

---
+++ src/main/java/com/daniil/swimcoach/swimaicoach/SwimaiCoachApplication.java
@@ -0,0 +1,13 @@
+package com.daniil.swimcoach.swimaicoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimaiCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwimaiCoachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniil/swimcoach/swimaicoach/SwimaiCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniil.swimcoach.swimaicoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimaiCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
