7c3949473d35bca8164cc177c1d09e4f9dc3446a
Daniil Dryzhov <danildryzov@gmail.com>
2026-01-18 15:21:23 +0100

    Create User and UserProfile entities with roles + added liquibase migrations

---
+++ src/main/java/com/daniil/swimcoach/swimaicoach/model/Role.java
@@ -0,0 +1,44 @@
+package com.daniil.swimcoach.swimaicoach.model;
+
+import com.daniil.swimcoach.swimaicoach.model.enums.RoleName;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true, length = 50)
+    private RoleName name;
+
+    @Column(length = 200)
+    private String description;
+
+    @Override
+    public String getAuthority() {
+        return name.name();
+    }
+}
---
+++ src/main/java/com/daniil/swimcoach/swimaicoach/model/enums/RoleName.java
@@ -0,0 +1,7 @@
+package com.daniil.swimcoach.swimaicoach.model.enums;
+
+public enum RoleName {
+    ROLE_FREE,
+    ROLE_PREMIUM,
+    ROLE_ADMIN
+}
